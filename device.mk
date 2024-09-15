28d74b924ce5fe7b73837135e392985a279d9f4c
ellenoire <ketchupnaiso@gmail.com>
2024-09-16 06:28:39 +0700

    add mount-android

--- device.mk
+++ device.mk
@@ -557,7 +557,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/halium-overlay/etc/init/ipa_fws.conf:system/halium/etc/init/ipa_fws.conf \
     $(LOCAL_PATH)/halium-overlay/etc/init/post-android-mount.conf:system/halium/etc/init/post-android-mount.conf \
     $(LOCAL_PATH)/halium-overlay/usr/etc/init.qcom.usb.rc:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/etc/init.qcom.usb.rc \
-    $(LOCAL_PATH)/halium-overlay/usr/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/bin/init.qcom.post_boot.sh 
+    $(LOCAL_PATH)/halium-overlay/usr/bin/init.qcom.post_boot.sh:$(TARGET_COPY_OUT_SYSTEM)/halium/usr/bin/init.qcom.post_boot.sh \
+    $(LOCAL_PATH)/halium-overlay/etc/init/mount-androi.conf:system/halium/etc/init/mount-android.conf \
 
 
 
